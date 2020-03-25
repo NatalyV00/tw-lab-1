3a0a37e7ac3cf9e5a894c01f5b55df41103bb234
Наталья Вырлан <vyrlan.natali@mail.ru>
2020-03-25 23:04:13 +0200

    BL

---
+++ Models/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyProject.Models
+{
+    public class UserLogin
+    {
+        public string Credential { get; set; }
+        public string Password { get; set; }
+    }
+}
